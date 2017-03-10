82afd32d836353df038950945b689e0776e81af8
Imran Mughal <imran.mugha87@gmail.com>
2017-03-10 10:20:46 +0000

    added html dynamic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,23 @@
 
 var cardOne = "queen", cardTwo = "queen", cardThree = "king", cardFour = "king";
 
-if (cardTwo === cardFour){
-	alert("You found a match!")
-} else {
-	alert("Sorry, try again.")
+// if (cardTwo === cardFour){
+// 	alert("You found a match!")
+// } else {
+// 	alert("Sorry, try again.")
+// }
+
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function(){
+	for (var i = 0; i < 4; i++){
+	
+	var cardElement = document.createElement('div')
+	cardElement.className = 'card';
+	
+	gameBoard.appendChild(cardElement);
 }
+}
+
+
+createCards();
